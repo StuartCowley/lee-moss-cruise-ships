170eae25c5445cdfc8036a409cb50374766cec9e
lamoss1 <109576407+lamoss1@users.noreply.github.com>
2022-12-29 17:08:10 +0000

    updated code

--- src/Ship.js
+++ src/Ship.js
@@ -1,5 +1,21 @@
-function Ship() {
-  this.startingPort = "Dover";
+class Ship {
+  constructor(currentPort) {
+    this.currentPort = currentPort;
+    this.previousPort = null;
+  }
+  setSail() {
+    this.previousPort = this.currentPort;
+    this.currentPort = null;
+  }
+  dock(port) {
+    this.currentPort = "Calais";
+  }
+}
+
+class Port {
+  constructor(name) {
+    this.name = name;
+  }
 }
 
 module.exports = Ship;
